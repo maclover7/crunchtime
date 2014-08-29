42c6cc9d8370ab15a6d370d417e995550f3f6837
Jon Moss <me@jonathanmoss.me>
2014-08-29 18:41:26 -0400

    user now owns courses and notes

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829223843) do
+ActiveRecord::Schema.define(version: 20140829224105) do
 
   create_table "courses", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "courses", ["user_id"], name: "index_courses_on_user_id"
 
   create_table "notes", force: true do |t|
     t.string   "title"
@@ -25,9 +28,11 @@ ActiveRecord::Schema.define(version: 20140829223843) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "course_id"
+    t.integer  "user_id"
   end
 
   add_index "notes", ["course_id"], name: "index_notes_on_course_id"
+  add_index "notes", ["user_id"], name: "index_notes_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
