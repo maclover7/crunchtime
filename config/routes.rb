f556fc0b6a4058f4b773d75df071f7bcb752135b
Jon Moss <me@jonathanmoss.me>
2014-09-01 08:58:00 -0400

    todo_items are nested in courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
-  resources :todo_items
-
   mount Bootsy::Engine => '/bootsy', as: 'bootsy'
+
   root "pages#home"
-  resources :courses
+
+  resources :courses do
+    resources :todo_items
+  end
+
   resources :notes
+  
   devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
